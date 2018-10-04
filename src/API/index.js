0fdd85e4357855c9891b60dab2b26c0ebfcf8292
Therese Tessman <therese.tessman@gmail.com>
2018-10-04 00:12:13 -0700

    added class helper

---
+++ src/API/index.js
@@ -0,0 +1,56 @@
+class Helper {
+
+	static baseURL() {
+			return "https://api.foursquare.com/v2";
+	}
+
+	static auth() {
+			const keys = {
+					client_id:"",
+					client_secret:"",
+					v:"20181003"
+			};
+			return object.keys(keys)
+				.map(key => `${key}=${keys[key]}`)
+				.join("&")
+	}
+
+	static urlBuilder(urlPrams) {
+		if(!urlPrams) {
+			return ""
+		}
+		return object.keys(urlPrams)
+			.map(key => `${key}=${urlPrams[key]}`)
+			.join("&")
+	}
+
+	static headers() {
+		return {
+			Accept:"application/json"
+		};
+	}
+
+	static simpleFetch(endPoint, method, urlPrams){
+		let requestData = {
+			method,
+			headers: Helper.headers()
+		};
+		return fetch(
+			`${Helper.baseURL()}${endPoint}?${Helper.auth()}&${Helper.urlBuilder(
+				urlPrams
+			)}`,
+			requestData
+		).then(res => res.json());
+	}
+}
+export default class SquareAPI {
+	static search(urlPrams) {
+		return Helper.simpleFetch("/venues/search", "GET", urlPrams);
+	}
+	static getVenueDetails(VENUE_ID) {
+		return Helper.simpleFetch(`/venues/${VENUE_ID}`, "GET");
+	}
+	static getVenuePhotos(VENUE_ID) {
+		return Helper.simpleFetch(`/venues/${VENUE_ID}/photos`, "GET");
+	}
+}
